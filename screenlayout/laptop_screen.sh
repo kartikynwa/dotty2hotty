db1bf24830d8ba845651958c5b73f2faaf9d6bde
Kartik Singh <kartik.ynwa@gmail.com>
2017-09-17 22:23:01 +0530

    f u c k x r a n d r

--- screenlayout/laptop_screen.sh
+++ screenlayout/laptop_screen.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output LVDS-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output HDMI-1 --off --output VGA-1 --off
+xrandr --output LVDS1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI1 --off --output VGA1 --off
