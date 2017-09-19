7aeaa630868fcbcaf799056b785e335e73a7c43c
Kartik Singh <kartik.ynwa@gmail.com>
2017-09-19 22:48:55 +0530

    added wallpapers

--- scripterino/wall.sh
+++ scripterino/wall.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
 # Script assigns random wallpaper when called.
-# Wallpapers need to be placed in ~/wallpapers/
+# Wallpapers need to be placed in ~/dotty2hotty/wallpapers/
 # Feh needs to be installed
 
-files=(~/wallpapers/*)
+files=(~/dotty2hotty/wallpapers/*)
 feh --bg-scale "${files[$RANDOM % ${#files[@]}]}"
